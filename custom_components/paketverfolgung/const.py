af799707103d682a8c7d6fb264addfaa76a8e65b
Jetiman <jetiman@googlemail.com>
2026-09-20 14:16:41 +0200

    release: stamp v1.15.0

--- custom_components/paketverfolgung/const.py
+++ custom_components/paketverfolgung/const.py
@@ -137,7 +137,7 @@ CARRIERS = (CARRIER_DHL, CARRIER_DPD, CARRIER_HERMES, CARRIER_UPS)
 # incl. any beta suffix; INTEGRATION_COMMIT is stamped by the release
 # commit (see the release flow) - "0000000" on an unreleased working tree.
 INTEGRATION_VERSION = "1.15.0"
-INTEGRATION_COMMIT = "972742b"
+INTEGRATION_COMMIT = "8a552d7"
 
 # Custom sidebar panel (buildless web component served from ./frontend).
 PANEL_URL_PATH = "paketverfolgung"
